81e5e9c0c923fba9f8fc15ddd418f2f37e63a04d
smoisset <smoisset@worldremit.com>
2018-06-11 19:10:19 +0100

    Create a Java Library

---
+++ javaJoker/src/main/java/com/sonyamoisset/android/javajoker/Joker.java
@@ -0,0 +1,8 @@
+package com.sonyamoisset.android.javajoker;
+
+public class Joker {
+
+    public String tellJoke() {
+        return "I am a nobody, nobody is perfect, therefore I am perfect.";
+    }
+}
